2f89b12e90cc669dedb190d169facb772c562c9d
Jhonatan <jonathandiazp@gmail.com>
2022-10-14 10:04:38 -0400

    freesql renewal

--- db/connection.js
+++ db/connection.js
@@ -1,12 +1,31 @@
 const Sequelize = require("sequelize");
-const sequelize = new Sequelize(
-  process.env.MYSQL_DATABASE_NAME,
-  process.env.MYSQL_USERNAME,
-  process.env.MYSQL_PASSWORD,
-  {
-    dialect: process.env.MYSQL_DIALECT,
-    host: process.env.MYSQL_HOST,
-  }
-);
+require('dotenv').config();
 
+let sequelize;
+if (process.env.JAWSDB_URL) {
+    sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+    sequelize = new Sequelize(
+        process.env.MYSQL_DATABASE_NAME,
+        process.env.MYSQL_USERNAME,
+        process.env.MYSQL_PASSWORD,
+        {
+          dialect: process.env.MYSQL_DIALECT,
+          host: process.env.MYSQL_HOST,
+        }
+      );
+}
 module.exports = sequelize;
+
+
+
+
+// const sequelize = new Sequelize(
+//   process.env.MYSQL_DATABASE_NAME,
+//   process.env.MYSQL_USERNAME,
+//   process.env.MYSQL_PASSWORD,
+//   {
+//     dialect: process.env.MYSQL_DIALECT,
+//     host: process.env.MYSQL_HOST,
+//   }
+// );
